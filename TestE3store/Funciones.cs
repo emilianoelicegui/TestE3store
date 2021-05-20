2ed70970168e00a0091becda2b291e74610d1904
Emiliano Elicegui <emielicegui@gmail.com>
2021-05-20 00:17:57 -0300

    Agregar archivos de proyecto.

---
+++ TestE3store/Funciones.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestE3store
+{
+    public class Funciones
+    {
+        class Fraccion
+        {
+            public Fraccion(int Numerador, int Denominador)
+            {
+                this.Numerador = Numerador;
+                this.Denominador = Denominador;
+            }
+
+            public int Numerador { get; }
+            public int Denominador { get; }
+        }
+
+        public bool validName(string nombre)
+        {
+            var nombreEspacios = nombre.Split(" ");
+
+            //c. dos nombres y un apellido
+            if (nombreEspacios.Length != 2 && nombreEspacios.Length != 3)
+                return false;
+
+            //d. nunca inicial primer nombre y no el segundo
+            if (nombreEspacios.Length == 3 && (nombreEspacios[1].Length != 2 || !nombreEspacios[1].EndsWith(".")))
+                return false;
+
+            //e. apellido siempre es una palabra
+            if (nombreEspacios.Length == 3 && (nombreEspacios[2].Length < 2 || nombreEspacios[2].EndsWith(".")))
+                return false;
+
+            foreach (var x in nombreEspacios)
+            {
+                //a. debe empezar con mayuscula 
+                if (!Char.IsUpper(x, 0))
+                    return false;
+
+                //b. terminar en .
+                if (x.Length < 2 || (x.Length == 2 && !x.EndsWith(".")))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public string simplificar(string fraccion)
+        {
+            var f = fraccion.Split("/");
+            var primos = new int[] { 2, 3, 5, 7 };
+
+            var n = Convert.ToInt32(f[0]);
+            var d = Convert.ToInt32(f[1]);
+
+            int n1 = n;
+            int d1 = d;
+
+            int cont = 0;
+
+            foreach(var p in primos)
+            {
+                try
+                {
+                    while (cont <= primos.Length)
+                    {
+                        fraccion = $"{n}/{d}";
+
+                        n1 = n;
+                        d1 = d;
+
+                        int r1, r2;
+
+                        n1 = Math.DivRem(n, p, out r1);
+                        d1 = Math.DivRem(d, p, out r2);
+
+                        if (r1 != 0 || r2 != 0)
+                            throw new Exception();
+
+                        n = n1;
+                        d = d1;
+                    }
+                }
+                catch
+                {
+                    cont++;
+
+                    continue;
+                }
+
+            }
+
+            return fraccion;
+        }
+    }
+}
---
+++ TestE3store/UnitTest1.cs
@@ -0,0 +1,77 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestE3store
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestValidName()
+        {
+            var funciones = new Funciones();
+
+            #region init 
+
+            Dictionary<string, bool> namesValue = new Dictionary<string, bool>();
+            
+            namesValue.Add("E. Poe", false);
+            namesValue.Add("E. A. Poe", false);
+            namesValue.Add("Edgard A. Poe", false);
+            namesValue.Add("Edgard", false);
+            namesValue.Add("e. Poe", false);
+            namesValue.Add("E Poe", false);
+            namesValue.Add("E. Allan Poe", false);
+            namesValue.Add("E. Allan P.", false);
+            namesValue.Add("Edg. Allan Poe", false);
+
+            #endregion init 
+
+            #region validate 
+            Dictionary<string, bool> results = new Dictionary<string, bool>();
+
+            foreach (var x in namesValue)
+            {
+                var valid = funciones.validName(x.Key);
+                results.Add(x.Key, valid);
+            }
+
+            #endregion validate 
+
+            #region results
+            
+            Dictionary<string, bool> valids = new Dictionary<string, bool>();
+
+            valids.Add("E. Poe", true);
+            valids.Add("E. A. Poe", true);
+            valids.Add("Edgard A. Poe", true);
+            valids.Add("Edgard", false);
+            valids.Add("e. Poe", false);
+            valids.Add("E Poe", false);
+            valids.Add("E. Allan Poe", false);
+            valids.Add("E. Allan P.", false);
+            valids.Add("Edg. Allan Poe", false);
+
+            #endregion results
+
+            Assert.IsTrue(!results.Except(valids).Any());
+        }
+
+        [TestMethod]
+        public void TestValidFraccion()
+        {
+            var funciones = new Funciones();
+
+            var f1 = "4/6";
+            var f2 = "10/11";
+            var f3 = "100/400";
+
+            var result1 = funciones.simplificar(f1);
+            var result2 = funciones.simplificar(f2);
+            var result3 = funciones.simplificar(f3);
+
+            Assert.IsTrue(result1 == "2/3" && result1 == "10/11" && result1 == "1/4");
+        }
+    }
+}
